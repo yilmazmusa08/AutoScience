2e6fb6208318f7fbee1bdd23e4279e3c64b35fe8
firengizz099 <121484651+firengizz099@users.noreply.github.com>
2023-06-25 14:57:39 +0300

    Add files via upload

---
+++ autoo.py
@@ -0,0 +1,293 @@
+import os
+import json
+import uvicorn
+import warnings
+import pandas as pd
+from fastapi import FastAPI, File, UploadFile
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings('ignore')
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', None)
+warnings.filterwarnings('ignore', category=ConvergenceWarning)
+from fastapi import FastAPI, Form, Request, Cookie
+from type_pred import *
+from sklearn.exceptions import ConvergenceWarning
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+from fastapi import HTTPException
+from fastapi_login import LoginManager
+from fastapi import FastAPI, Request, Depends, Form, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+from fastapi.security import OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+import traceback
+import sys
+from jose import JWTError, jwt
+
+models_path = '/home/firengiz/Belgeler/proje/automl/models'
+# models_path'i sys.path listesine ekle
+sys.path.append(models_path)
+from init import create_model
+
+app = FastAPI()
+
+SECRET = "supersecret"
+manager = LoginManager(SECRET, "/login")
+
+# Veritabanı bağlantısı oluşturma
+SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Create a password context
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def update_passwords():
+    db = SessionLocal()
+    try:
+        users = db.query(User).all()
+        for user in users:
+            if not pwd_context.identify(user.password):
+                # If the password is not bcrypt hashed, update it
+                hashed_password = pwd_context.hash(user.password)
+                user.password = hashed_password
+        db.commit()
+        print("Password update successful")
+    except Exception as e:
+        print("Password update failed:", str(e))
+    finally:
+        db.close()
+
+update_passwords()
+
+# Template dosyalarını yükleme
+templates = Jinja2Templates(directory="template")
+
+# Statik dosyaları sunma
+app.mount("/dist", StaticFiles(directory="dist"), name="dist")
+app.mount("/plugins", StaticFiles(directory="plugins"), name="plugins")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(
+    request: Request,
+    signup_success: bool = False,
+    message: str = "",
+    email: str = ""
+):
+    is_logged_in = bool(email)
+    logo_button = (
+        f'<a class="nav-link" href="#" style="display: block;">'
+        f'<div class="logo-button">{email[0].upper()}</div></a>'
+    ) if is_logged_in else ''
+    context = {
+        "request": request,
+        "signup_success": signup_success,
+        "message": message,
+        "is_logged_in": is_logged_in,
+        "logo_button": logo_button,
+        "is_logged_in_js": str(is_logged_in).lower(),
+    }
+    return templates.TemplateResponse("home.html", context)
+
+
+
+@app.post("/register")
+def register(request: Request, email: str = Form(...), password: str = Form(...), db: SessionLocal = Depends(get_db)):
+    # Check if the user already exists
+    user = db.query(User).filter(User.email == email).first()
+    if user:
+        print("User already exists")
+        return Response(status_code=409, content="User already exists")
+    
+    # Register the user
+    hashed_password = pwd_context.hash(password)  # Şifreyi bcrypt ile şifrele
+    new_user = User(email=email, password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    
+    print("Registration successful")
+    return Response(status_code=200, content="Registration successful")
+
+
+def requires_login(function):
+    async def wrapper(*args, **kwargs):
+        request = next(arg for arg in args if isinstance(arg, Request))
+        access_token = request.cookies.get("access_token")
+
+        if not access_token:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        # Add your token verification logic here
+        # For example, decode and verify the access token
+
+        # If the token is valid, proceed with the wrapped function
+        return await function(*args, **kwargs)
+
+    return wrapper
+
+@app.post("/login")
+def login(request: Request, data: OAuth2PasswordRequestForm = Depends(), db: SessionLocal = Depends(get_db)):
+    email = data.username
+    password = data.password
+
+    # Find the user with the given email
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user or not pwd_context.verify(password, user.password):
+        print("Invalid email or password")
+        return templates.TemplateResponse("home.html", {"request": request, "message": "Invalid email or password"})
+
+    # User is authenticated, generate and return the access token
+    token = manager.create_access_token(data=dict(sub=email))
+    print("Generated access token:", token)  # Print the generated access token
+
+    print("Login successful for user:", email)
+    # Set the access token as a cookie and redirect to the homepage
+    response = RedirectResponse(url="/process", status_code=302)
+    response.set_cookie(key="access_token", value=token, httponly=True, max_age=3600)
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Login successful"})
+
+async def get_current_user(request: Request, token: str = Cookie(None), db: SessionLocal = Depends(get_db)):
+    print("Access token:", token)  # Access token'ı kontrol etmek için
+    if not token:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    try:
+        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
+        print("Decoded payload:", payload)  # Decode edilmiş payload'ı kontrol etmek için
+        user_id = payload.get("sub")
+        print("User ID:", user_id)  # Kullanıcı kimliğini kontrol etmek için
+        if user_id:
+            user = db.query(User).filter(User.email == user_id).first()
+            if user:
+                return user
+    except JWTError as e:
+        print("JWTError:", str(e))  # JWT hatasını kontrol etmek için
+
+    raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+def is_user_subscribed(user: User):
+    # Kullanıcının abone olup olmadığını kontrol eden bir işlev tanımlayın.
+    # Abone ise True, değilse False döndürün.
+
+    # Örnek bir abonelik kontrolü:
+    if user.subscription_status == "active":
+        return True
+    else:
+        return False
+    
+@app.post("/", response_class=HTMLResponse)
+async def run_analysis_api(
+    request: Request,
+    file: UploadFile = File(...),
+    target: str = Form(...),
+    warning: bool = Form(True),
+    return_stats: bool = Form(False),
+    comp_ratio: float = Form(1.0),
+    current_user: User = Depends(get_current_user)
+):
+    # Check if the user is subscribed
+    if not is_user_subscribed(current_user):
+        print("Access token found:", current_user.email)
+        raise HTTPException(status_code=403, detail="Not subscribed")
+    else:
+        print("Login Status : ", is_user_subscribed(current_user))
+
+    df = pd.read_csv(file.file)
+
+    sonuc = analysis(df, target=target, warning=warning, return_stats=return_stats)
+
+    pca_dict = {}
+    for col in sonuc['Role']:
+        null_counts = df.isnull().sum()
+        empty_cols = null_counts[null_counts >= len(df) * 0.6].index
+        df.drop(empty_cols, axis=1, inplace=True)
+
+        for col in df.columns:
+            if df[col].dtype == 'object' and df[col].nunique() < 20:
+                df[col].fillna(df[col].mode()[0], inplace=True)
+                le = LabelEncoder()
+                df[col] = le.fit_transform(df[col])
+            elif df[col].dtype != 'object':
+                df[col].fillna(df[col].mean(), inplace=True)
+
+        df[col] = df[col].fillna(df[col].mean())
+        result_dict = calculate_pca(df.select_dtypes(include=['float', 'int']), comp_ratio=comp_ratio)
+        pca_dict = {
+            'Cumulative Explained Variance Ratio': result_dict['Cumulative Explained Variance Ratio'],
+            'Principal Component': result_dict['Principal Component']
+        }
+
+    sonuc['PCA'] = pca_dict
+    sonuc = set_to_list(sonuc)
+    output_dict = {"result": sonuc}
+
+    # After analysis, store the output in a JSON file (output.json in this example)
+    with open("analysis.json", "w") as f:
+        json.dump(output_dict, f)
+
+    # Render the HTML template
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+@app.post("/model", response_class=HTMLResponse)
+async def run_models(request: Request, file: UploadFile = File(...), date: str = Form(None), target: str = Form(None), forecast: int = Form(None)):
+    try:
+        df = pd.read_csv(file.file)
+
+        sonuc = create_model(df, date=date, forecast=forecast, target=target) if date else create_model(df, date=None,forecast=None,target=target)
+        
+        # Sonucu liste olarak dönüştür
+        sonuc = list(sonuc)
+
+        output_file = os.path.join(os.getcwd(), "model.json")
+
+        with open(output_file, "w") as f:
+            json.dump(sonuc, f)
+
+        # '/model' endpointine yönlendir
+        return RedirectResponse(url="/model", status_code=302)
+    except Exception as e:
+        traceback.print_exc()
+        return f"Dataset kabul edilmedi. Bir hata oluştu: {str(e)}"
+
+
+
+@app.get("/model", response_class=HTMLResponse)
+async def run_process2(request: Request):
+    try:
+        with open("model.json", "r") as f:
+            output_dict2 = json.load(f)
+
+        # HTML şablonunu renderle
+        return templates.TemplateResponse("index.html", {"request": request, "result": json.dumps(output_dict2)})
+    except Exception as e:
+        return f"Veri setini işlerken bir hata oluştu: {str(e)}"
+
+
+
+uvicorn.run(app, host="127.0.0.1", port=5050)
+
